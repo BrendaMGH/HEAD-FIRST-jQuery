52adca3902d493bc8732bd21653b91172aaea118
Brenda <bmgonzalezh@uqvirtual.edu.co>
2020-09-04 10:41:39 -0500

    MonsterMashup Chapter 7

--- MonsterMashup/scripts/my_scripts.js
+++ MonsterMashup/scripts/my_scripts.js
@@ -1,59 +1,80 @@
 $(document).ready(function () {
-    var headclix = 0, eyesclix = 0, noseclix = 0, mouthclix = 0;
-    lightning_one(4000);
-    lightning_two(5000);
-    lightning_three(7000);
+    var clix = [0, 0, 0, 0];
+    var int1, int2, int3;
+
+    goLightning();
+    window.onblur = stopLightning;
+    window.onfocus = goLightning;
+
     $("#head").click(function () {
-        if (headclix < 9) {
-            headclix++;
-            $(this).animate({ left: "-=367px"},500);
-        } else {
-            headclix = 0;
-            $("#head").animate({ left: "0px"},500);
-        }
+        moveMe(0, this);
     });
     $("#eyes").click(function () {
-        if (eyesclix < 9) {
-            eyesclix++;
-            $("#eyes").animate({ left: "-=367px"},500);
-        } else {
-            eyesclix = 0;
-            $("#eyes").animate({ left: "0px"},500);
-        }
+        moveMe(1, this);
     });
     $("#nose").click(function () {
-        if (noseclix < 9) {
-            noseclix++;
-            $("#nose").animate({ left: "-=367px"},500);
-        } else {
-            noseclix = 0;
-            $("#nose").animate({ left: "0px"},500);
-        }
+        moveMe(2, this);
     });
     $("#mouth").click(function () {
-        if (mouthclix < 9) {
-            mouthclix++;
-            $("#mouth").animate({ left: "-=367px"},500);
-        } else {
-            mouthclix = 0;
-            $("#mouth").animate({ left: "0px"},500);
-        }
+        moveMe(3, this);
     });
+
+    $("#btnRandom").click(randomize);
+
+    $("#btnReset").click(reset);
+
+    // Functions
+    function getRandom(num) {
+        var my_random_num = Math.floor(Math.random() * num);
+        return my_random_num;
+    }
+    function randomize() {
+        var target_position;
+        var move_to; 
+        $(".face").each(function (index) {
+            target_position = getRandom(10);
+            clix[index] = target_position;
+            move_to = target_position * 367;
+            $(this).animate({ left: "-" + move_to + "px" }, 500);
+        });
+    };
+
+    function reset() {
+        $(".face").each(function (index) {
+            clix[index] = 0;
+            $(this).animate({left: "0px"},500);
+        })
+    }
+
+    function moveMe(index, element) {
+        if (clix[index] < 9) {
+            clix[index]++;
+            $(element).animate({ left: "-=367px" }, 500);
+        } else {
+            clix[index] = 0;
+            $(element).animate({ left: "0px" }, 500);
+        }
+    }
+
+    function goLightning() {
+        int1 = setInterval(lightning_one, 4000);
+        int2 = setInterval(lightning_two, 5000);
+        int3 = setInterval(lightning_three, 7000);
+    }
+    function stopLightning() {
+        window.clearInterval(int1);
+        window.clearInterval(int2);
+        window.clearInterval(int3);
+    }
+
+    function lightning_one(t) {
+        $("#lightning1").fadeIn(250).fadeOut(250);
+    };
+    function lightning_two(t) {
+        $("#lightning2").fadeIn(250).fadeOut(250);
+    };
+    function lightning_three(t) {
+        $("#lightning3").fadeIn(250).fadeOut(250);
+    };
 });
 
-function lightning_one(t) {
-    $("#lightning1").fadeIn(250).fadeOut(250);
-    setTimeout("lightning_one()", t);
-};
-function lightning_two(t) {
-    $("#lightning2").fadeIn(250).fadeOut(250);
-    setTimeout("lightning_two()", t);
-};
-function lightning_three(t) {
-    $("#lightning3").fadeIn(250).fadeOut(250);
-    setTimeout("lightning_three()", t);
-};
-function lightning_four(t) {
-    $("#lightning4").fadeIn(250).fadeOut(250);
-    setTimeout("lightning_four()", t);
-};
--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Use jQuery **selctors, methods and _$(this)_**, create Jump for Joy page. [See h
 ### Chapter 3 - JumpForJoy
 Use an external file for jQuery, use new methods -> _**addClass, removeClass, hover**_  and create **functions**. [See here](../../tree/a161365b0e28a41fdcdd72d280e7b3814c4f0894) 
 ### Chapter 4 - Eatery
-**Manipulate the DOM -> _remove detach before after_**, use methods to relate elements, filters and save HTML elements in arrays. [See here](../../tree/e876a6da7593e2ee2af0ca5157a65b4ba72787a1)
+**Manipulate the DOM -> _remove, detach, before, after_**, use methods to relate elements, filters and save HTML elements in arrays. [See here](../../tree/e876a6da7593e2ee2af0ca5157a65b4ba72787a1)
 ### Chapter 5 - MonsterMashup
 **Add animations _fadeIn slide toogle_** and create custom animations -> **_animate_**. [See here](../../tree/c31cae7513245691f3dc86dbf2b59aa38185abf6) 
 ### Chapter 6 - BlackJack
-Use **Javascript** objects, arrays and loops, more jQuery methods.
+Use **Javascript** objects, arrays and loops, more jQuery methods. [See here](../../tree/edcf0999c86ea76afe209d49fdac3b88f5cd4e3a) 
+### Chapter 7 - MonsterMashup
+Use Javascript _**window**_ object and its methods and restructure the code.  
